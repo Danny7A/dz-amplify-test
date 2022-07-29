2372f2dd3d2d00bee3c766d4648960e8e3bd1957
Daniel Bekele <danukiya@live.se>
2022-07-29 14:57:23 +0200

    Modified Readme and app.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <h1>Danukiya</h1> <a href="https://www.danukiya.dz"></a>
         </p>
         <a
           className="App-link"
